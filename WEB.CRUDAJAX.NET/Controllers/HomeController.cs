9e2490679c64f4371eba910751551eab04432f97
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-12-11 11:10:46 -0300

    Creado modelo y agregado los metodos CRUD en el controlador

--- WEB.CRUDAJAX.NET/Controllers/HomeController.cs
+++ WEB.CRUDAJAX.NET/Controllers/HomeController.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB.CRUDAJAX.NET.Models;
 
 namespace WEB.CRUDAJAX.NET.Controllers
 {
     public class HomeController : Controller
     {
+        BDPRUEBASEntities ctx = new BDPRUEBASEntities();
+
+        public ActionResult EliminarCliente(int id)
+        {
+            Customers eliminar = (from e in ctx.Customers where e.CustomerId == id select e).FirstOrDefault();
+
+            ctx.Customers.Remove(eliminar);
+            ctx.SaveChanges();
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult EditarCliente(Customers cliente)
+        {
+            Customers actualizar = (from c in ctx.Customers where c.CustomerId == cliente.CustomerId select c).FirstOrDefault();
+
+            actualizar.Country = cliente.Country;
+            actualizar.Name = cliente.Name;
+            ctx.SaveChanges();
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult CrearCliente(Customers cliente)
+        {
+            ctx.Customers.Add(cliente);
+            ctx.SaveChanges();
+
+            return Json(cliente);
+        }
+
+
+
+
+
         public ActionResult Index()
         {
-            return View();
-        }
+            return View(ctx.Customers);
+        }     
 
         public ActionResult About()
         {
